b8756d19e3cb2232b6db845799f0c35f6f83426e
mmfajri <mmathlailfajri@gmail.com>
2025-11-11 15:12:31 +0700

    refactor : product repository

--- pos_api_app/Repositories/Entities/ProductRepository.cs
+++ pos_api_app/Repositories/Entities/ProductRepository.cs
@@ -34,7 +34,7 @@ public class ProductRepository : GeneralRepository<Product>, IProductRepository
 		var connection = _posDbContext.Database.GetDbConnection();
 
 		var parameters = new DynamicParameters();
-		var query = @"SELECT product.barcode_id BARCODE_ID, product.title TITLE, unit.Name UNIT, price.amount PRICE 
+		var query = @"SELECT product.barcode_id BarcodeId, product.title TITLE, unit.Name UNIT, price.amount Amount 
                   FROM tb_tr_price price
                   JOIN tb_m_product product on price.product_id = product.id
                   JOIN tb_m_unit unit on price.unit_id = unit.id 
